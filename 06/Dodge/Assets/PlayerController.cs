069673a1a725d5ce2a12f512d45992e4669d45d9
vetan2 <vetan2@naver.com>
2022-05-30 23:14:24 +0900

    6.4

---
+++ 06/Dodge/Assets/PlayerController.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody playerRigidbody;
+
+    public float speed = 8f;
+    
+    void Start()
+    {
+    }
+    
+    void Update()
+    {
+    }
+}
