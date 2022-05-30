2d1f14e68ba3c1e821b271681dfd112b47b6cf86
vetan2 <vetan2@naver.com>
2022-05-30 23:23:08 +0900

    6.5.2

--- 06/Dodge/Assets/PlayerController.cs
+++ 06/Dodge/Assets/PlayerController.cs
@@ -7,12 +7,31 @@ public class PlayerController : MonoBehaviour
     public Rigidbody playerRigidbody;
 
     public float speed = 8f;
-    
+
     void Start()
     {
     }
-    
+
     void Update()
     {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            playerRigidbody.AddForce(0f, 0f, speed);
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            playerRigidbody.AddForce(0f, 0f, -speed);
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            playerRigidbody.AddForce(speed, 0f, 0f);
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow) == true)
+        {
+            playerRigidbody.AddForce(-speed, 0f, 0f);
+        }
     }
 }
