44d9bffd9c1ef1bc81187b2e51867d0b22099d1e
vetan2 <vetan2@naver.com>
2022-06-02 21:20:45 +0900

    7.2.1

---
+++ 06/Dodge/Assets/Bullet.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float speed = 8f;
+
+    private Rigidbody bulletRigidbody;
+    
+    void Start()
+    {
+    }
+}
